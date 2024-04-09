9c7f6a7ce603d1448a622c680d05a69eb5fa6c34
Stuart Murray <stuart.murray.dev@outlook.com>
2024-04-09 17:41:28 +0100

    mui

---
+++ code/48_material-ui-demo/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import AlarmIcon from "@mui/icons-material/Alarm";
+import RatingDemo from "./RatingDemo";
+import FormDemo from "./FormDemo";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <FormDemo />
+    </div>
+  );
+}
+
+export default App;
